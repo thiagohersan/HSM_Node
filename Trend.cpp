860a0c40944693697a76c0a58c52dbc6113f4e7d
Thiago Hersan <thiago.hersan+github@gmail.com>
2017-10-31 15:37:55 -0200

    using new color palette

--- Trend.cpp
+++ Trend.cpp
@@ -1,24 +1,6 @@
 #include "Trend.h"
 
-uint32_t Trend::allColors[] = { Adafruit_NeoPixel::Color(111, 40, 104),
-                                Adafruit_NeoPixel::Color(62, 71, 220),
-                                Adafruit_NeoPixel::Color(47, 93, 183),
-                                Adafruit_NeoPixel::Color(20, 122, 188),
-                                Adafruit_NeoPixel::Color(31, 113, 147),
-                                Adafruit_NeoPixel::Color(15, 132, 112),
-                                Adafruit_NeoPixel::Color(0, 153, 76),
-                                Adafruit_NeoPixel::Color(52, 173, 62),
-                                Adafruit_NeoPixel::Color(153, 215, 31),
-                                Adafruit_NeoPixel::Color(203, 235, 15),
-                                Adafruit_NeoPixel::Color(255, 248, 0),
-                                Adafruit_NeoPixel::Color(255, 204, 1),
-                                Adafruit_NeoPixel::Color(255, 152, 0),
-                                Adafruit_NeoPixel::Color(255, 102, 1),
-                                Adafruit_NeoPixel::Color(235, 52, 56),
-                                Adafruit_NeoPixel::Color(255, 52, 0)
-                              };
-
-uint8_t Trend::numColors = sizeof(Trend::allColors) / sizeof(uint32_t);
+uint32_t Trend::temperatureColors[] = { 0x36a9fe, 0xff4399 };
 
 Trend::Trend() {
   lerpPosition = 1.0;
@@ -41,11 +23,12 @@ void Trend::update() {
 }
 void Trend::setColor(float percent) {
   lerpColors[0] = lerpColors[1];
-  lerpColors[1] = allColors[constrain((int)(percent * numColors), 0, numColors - 1)];
+  lerpColors[1] = lerpColor(temperatureColors[0], temperatureColors[1], percent);
   lerpPosition = 0.0;
 }
 
 uint32_t Trend::lerpColor(uint32_t c0, uint32_t c1, float pos) {
+  pos = constrain(pos, 0.0, 1.0);
   return Adafruit_NeoPixel::Color((uint8_t) lerp((c0 >> 16) & 0xff, (c1 >> 16) & 0xff, pos),
                                   (uint8_t) lerp((c0 >> 8) & 0xff, (c1 >> 8) & 0xff, pos),
                                   (uint8_t) lerp((c0 >> 0) & 0xff, (c1 >> 0) & 0xff, pos));
--- Trend.h
+++ Trend.h
@@ -10,8 +10,7 @@ class Trend {
     uint32_t lerpColor(uint32_t c0, uint32_t c1, float pos);
     float lerp(float y0, float y1, float pos);
     Adafruit_NeoPixel *mPixels;
-    static uint32_t allColors[];
-    static uint8_t numColors;
+    static uint32_t temperatureColors[];
   public:
     Trend();
     void update();
