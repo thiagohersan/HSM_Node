5001d02d0683e21aac0bdcd20e26d0ef694589bf
Thiago Hersan <thiago.hersan+github@gmail.com>
2018-01-20 01:24:04 -0200

    only declare functions in .h file

--- utils.h
+++ utils.h
@@ -1,33 +1,4 @@
-void reset() {
-  ESP.deepSleep(500e3);
-}
-
-void checkForNewBinary() {
-  t_httpUpdate_return ret = ESPhttpUpdate.update(BINARY_SERVER_ADDRESS, BINARY_SERVER_PORT, BINARY_SERVER_ENDPOINT, "", false, "", false);
-  switch (ret) {
-    case HTTP_UPDATE_FAILED:
-      Serial.println("[UPDATE] failed.");
-      break;
-    case HTTP_UPDATE_NO_UPDATES:
-      Serial.println("[UPDATE] NO update.");
-      break;
-    case HTTP_UPDATE_OK:
-      Serial.println("[UPDATE] OK.");
-      needsReset = true;
-      break;
-  }
-}
-
-void setupAndStartOTA() {
-  ArduinoOTA.setHostname(OTA_HOSTNAME.c_str());
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
-  });
-  ArduinoOTA.begin();
-}
+void reset();
+void checkForNewBinary();
+void setupAndStartOTA();
 
