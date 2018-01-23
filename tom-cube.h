dcb18ed16fb911db93b9eab207a0d40a2f102289
Thiago Hersan <thiago.hersan+github@gmail.com>
2018-01-23 15:19:28 -0200

    moves helper functions into a header file

---
+++ tom-cube.h
@@ -0,0 +1,52 @@
+String BINARY_VERSION = "deadbeef";
+String BINARY_SERVER_ENDPOINT = "/bin/" + BINARY_VERSION;
+
+String OTA_HOSTNAME = "ToM-";
+String TREND = "";
+
+bool needsReset = false;
+
+void checkForNewBinary() {
+  t_httpUpdate_return ret = ESPhttpUpdate.update(BINARY_SERVER_ADDRESS, BINARY_SERVER_PORT, BINARY_SERVER_ENDPOINT, "", false, "", false);
+  switch (ret) {
+    case HTTP_UPDATE_FAILED:
+      Serial.println("[UPDATE] failed.");
+      break;
+    case HTTP_UPDATE_NO_UPDATES:
+      Serial.println("[UPDATE] NO update.");
+      break;
+    case HTTP_UPDATE_OK:
+      Serial.println("[UPDATE] OK.");
+      needsReset = true;
+      break;
+  }
+}
+
+void setupAndStartOTA() {
+  ArduinoOTA.setHostname(OTA_HOSTNAME.c_str());
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+}
+
+int find(int arr[], int item) {
+  int arrSize = sizeof(arr)/sizeof(arr[0]);
+
+  for (int i = 0; i < arrSize; i++) {
+    if (arr[i] == item) {
+      return i;
+    }
+  }
+  return arrSize;
+}
+
+void reset() {
+  ESP.deepSleep(500e3);
+}
+
--- tom-cube.ino
+++ tom-cube.ino
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-#include <EEPROM.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
@@ -9,23 +8,25 @@
 
 #include <Adafruit_NeoPixel.h>
 
-#include "Trend.h"
 #include "parameters.h"
+#include "tom-cube.h"
+#include "Trend.h"
 
 int TREND_ORDER[] = {7, 12, 18, 8, 4, 21, 24, 22, 16, 2, 17, 11, 9, 10, 23, 5, 3, 1, 15, 20, 13, 6, 14, 19};
 int TREND_ORDER_SIZE = sizeof(TREND_ORDER)/sizeof(TREND_ORDER[0]);
 
-String BINARY_VERSION = "deadbeef";
-String OTA_HOSTNAME = "ToM-";
-String TREND = "";
-
-String BINARY_SERVER_ENDPOINT = "/bin/" + BINARY_VERSION;
-
 long SLEEP_MILLIS = 30e3L;
 long nextUpdate = 0L;
-bool needsReset = false;
 
 Trend mTrend;
+
+void update() {
+  int found_index = find(TREND_ORDER, TREND.toInt());
+  if (found_index <= TREND_ORDER_SIZE) {
+    float colorPercent = (float(TREND_ORDER_SIZE) - found_index) / float(TREND_ORDER_SIZE);
+    mTrend.setColor(colorPercent);
+  }
+}
 
 void setup() {
   Serial.begin(115200);
@@ -47,61 +48,16 @@ void setup() {
   checkForNewBinary();
 }
 
-int find(int arr[], int arrSize, int item) {
-  for (int i = 0; i < arrSize; i++) {
-    if (arr[i] == item) {
-      return i;
-    }
-  }
-  return arrSize;
-}
-
 void loop() {
   if (needsReset) reset();
 
   if (millis() > nextUpdate) {
-    int found_index = find(TREND_ORDER, TREND_ORDER_SIZE, TREND.toInt());
-    if (found_index <= TREND_ORDER_SIZE) {
-      float colorPercent = (float(TREND_ORDER_SIZE) - found_index) / float(TREND_ORDER_SIZE);
-      mTrend.setColor(colorPercent);
-    }
+    update();
+    checkForNewBinary();
     nextUpdate += SLEEP_MILLIS;
   }
-  digitalWrite(2, ((nextUpdate / SLEEP_MILLIS) % 2));
+  digitalWrite(2, ((nextUpdate / SLEEP_MILLIS / 30) % 2));
 
   ArduinoOTA.handle();
-}
-
-void reset() {
-  ESP.deepSleep(500e3);
-}
-
-void checkForNewBinary() {
-  t_httpUpdate_return ret = ESPhttpUpdate.update(BINARY_SERVER_ADDRESS, BINARY_SERVER_PORT, BINARY_SERVER_ENDPOINT, "", false, "", false);
-  switch (ret) {
-    case HTTP_UPDATE_FAILED:
-      Serial.println("[UPDATE] failed.");
-      break;
-    case HTTP_UPDATE_NO_UPDATES:
-      Serial.println("[UPDATE] NO update.");
-      break;
-    case HTTP_UPDATE_OK:
-      Serial.println("[UPDATE] OK.");
-      needsReset = true;
-      break;
-  }
-}
-
-void setupAndStartOTA() {
-  ArduinoOTA.setHostname(OTA_HOSTNAME.c_str());
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
-  });
-  ArduinoOTA.begin();
 }
 
