fe38513f744311cea4f3c49ea4a90138331a9097
Thiago Hersan <thiago.hersan+github@gmail.com>
2018-01-23 15:31:27 -0200

    checks for new binaries every 2 minutes

--- tom-cube.ino
+++ tom-cube.ino
@@ -8,62 +8,19 @@
 
 #include <Adafruit_NeoPixel.h>
 
-#include "Trend.h"
 #include "parameters.h"
-
-String BINARY_VERSION = "deadbeef";
-
-String OTA_HOSTNAME = "ToM-";
-String TREND = "";
-
-String BINARY_SERVER_ENDPOINT = "/bin/" + BINARY_VERSION;
-String TREND_SERVER_ENDPOINT = "/panel/cubes/";
-
-long SLEEP_MILLIS = 5e3L;
-long nextUpdate = 0L;
-bool needsReset = false;
+#include "tom-cube.h"
+#include "Trend.h"
 
 Trend mTrend;
-
-void reset() {
-  ESP.deepSleep(500e3);
-}
-
-void checkForNewBinary() {
-  t_httpUpdate_return ret = ESPhttpUpdate.update(BINARY_SERVER_ADDRESS, BINARY_SERVER_PORT, BINARY_SERVER_ENDPOINT, "", false, "", false);
-  switch (ret) {
-    case HTTP_UPDATE_FAILED:
-      Serial.println("[UPDATE] failed.");
-      break;
-    case HTTP_UPDATE_NO_UPDATES:
-      Serial.println("[UPDATE] NO update.");
-      break;
-    case HTTP_UPDATE_OK:
-      Serial.println("[UPDATE] OK.");
-      needsReset = true;
-      break;
-  }
-}
-
-void setupAndStartOTA() {
-  ArduinoOTA.setHostname(OTA_HOSTNAME.c_str());
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
-  });
-  ArduinoOTA.begin();
-}
 
 void setup() {
   Serial.begin(115200);
   Serial.println("\n");
   pinMode(2, OUTPUT);
 
-  nextUpdate = millis() + SLEEP_MILLIS;
+  nextTrendUpdate = millis();
+  nextBinaryUpdate = millis();
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID.c_str(), WIFI_PASS.c_str());
@@ -76,10 +33,9 @@ void setup() {
   TREND_SERVER_ENDPOINT += TREND;
 
   setupAndStartOTA();
-  checkForNewBinary();
 }
 
-void update() {
+void updateTrend() {
   if (WiFi.status() != WL_CONNECTED) return;
 
   HTTPClient http;
@@ -99,11 +55,17 @@ void update() {
 void loop() {
   if (needsReset) reset();
 
-  if (millis() > nextUpdate) {
-    update();
-    nextUpdate += SLEEP_MILLIS;
+  if (millis() > nextTrendUpdate) {
+    updateTrend();
+    nextTrendUpdate += TREND_UPDATE_PERIOD_MILLIS;
   }
-  digitalWrite(2, (nextUpdate / SLEEP_MILLIS) % 2);
+
+  if (millis() > nextBinaryUpdate) {
+    checkForNewBinary();
+    nextBinaryUpdate += BINARY_UPDATE_PERIOD_MILLIS;
+  }
+
+  digitalWrite(2, (nextTrendUpdate / TREND_UPDATE_PERIOD_MILLIS) % 2);
   ArduinoOTA.handle();
 }
 
